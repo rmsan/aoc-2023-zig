e3fd942c1e0d7a1dbb70083e45a6caa92e9d23fc
Ramón Miguel Sanchez <1803314+rmsan@users.noreply.github.com>
2023-12-01 20:34:16 +0000

    Add first day

---
+++ day01/main.zig
@@ -0,0 +1,79 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const fileContent = @embedFile("input.txt");
+    var lines = std.mem.tokenizeAny(u8, fileContent, "\n");
+
+    var part1: usize = 0;
+    var part2: usize = 0;
+    while (lines.next()) |line| {
+        part1 += try solvePart1(line);
+        part2 += try solvePart2(line);
+    }
+
+    std.debug.print("Part1: {d}\nPart2: {d}\n", .{ part1, part2 });
+}
+
+fn solvePart1(input: []const u8) !usize {
+    var firstDigit: ?u8 = null;
+    var lastDigit: ?u8 = null;
+    for (input) |char| {
+        if (char < '0' or char > '9') {
+            continue;
+        }
+        const digit = char - '0';
+        if (firstDigit == null) {
+            firstDigit = digit;
+        }
+        lastDigit = digit;
+    }
+
+    return 10 * firstDigit.? + lastDigit.?;
+}
+
+const NUMBERS = [_][]const u8{ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+fn solvePart2(input: []const u8) !usize {
+    var firstDigit: ?usize = null;
+    var lastDigit: ?usize = null;
+    for (input, 0..) |char, i| {
+        var digit: ?usize = null;
+        if (char < '0' or char > '9') {
+            for (NUMBERS, 1..) |numberChar, j| {
+                if (std.mem.startsWith(u8, input[i..], numberChar)) {
+                    digit = j;
+                }
+            }
+        } else {
+            digit = char - '0';
+        }
+
+        if (digit) |realDigit| {
+            if (firstDigit == null) {
+                firstDigit = realDigit;
+            }
+            lastDigit = realDigit;
+        }
+    }
+
+    return 10 * firstDigit.? + lastDigit.?;
+}
+
+test "test-input" {
+    const fileContentTest1 = @embedFile("test1.txt");
+    var lines1 = std.mem.tokenizeAny(u8, fileContentTest1, "\n");
+    var part1: usize = 0;
+    while (lines1.next()) |line| {
+        part1 += try solvePart1(line);
+    }
+
+    const fileContentTest2 = @embedFile("test2.txt");
+    var lines2 = std.mem.tokenizeAny(u8, fileContentTest2, "\n");
+    var part2: usize = 0;
+    while (lines2.next()) |line| {
+        part2 += try solvePart2(line);
+    }
+
+    try std.testing.expectEqual(part1, 142);
+    try std.testing.expectEqual(part2, 281);
+}
